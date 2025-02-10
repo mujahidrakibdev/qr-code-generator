0b1fd17cd8f92581e4497d6d2f4285de18d01c14
Mujahid <mujahidrakibdev@gmail.com>
2025-02-10 10:15:01 +0600

    fully completed.

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+const container = document.querySelector(".container")
 const data = document.querySelector(".data-input")
 const image = document.querySelector(".image-input")
 const bgColor = document.querySelector(".bg-color-input")
-const margin = document.querySelector(".margin-input")
-const height = document.querySelector(".height-input")
-const width = document.querySelector(".width-input")
+const marginInput = document.querySelector(".margin-input")
+const heightInput = document.querySelector(".height-input")
+const widthInput = document.querySelector(".width-input")
 const dotOptions = document.querySelector(".dot-options")
 const dotColor = document.querySelector(".dot-color")
 const squareOptions = document.querySelector(".square-options")
@@ -13,22 +14,58 @@ const cornerDotColor = document.querySelector(".corner-dot-color")
 const generateBtn = document.querySelector(".generate-qr")
 const qrContainer = document.querySelector(".qr-container")
 const imageInput = document.querySelector(".image-input")
+const errorMessage = document.querySelector(".error-message")
+const uploadMessage = document.querySelector(".upload-message")
 const downloadBtn = document.querySelector(".download-btn")
 
-let qrCode;
+
+
+
+let qrCode; 
 let uploadedImageURL = ""
 
 generateBtn.addEventListener("click", generateQrCode)
-// downloadBtn.addEventListener("click", downloadQrCode)
 
 
 function generateQrCode() {
     qrContainer.innerHTML = ""
+    hideError()
+
+    if (!data.value.trim()) {
+        shake()
+        showError("QR data cannot be empty.")
+        return 
+    }
+
+    const width = parseInt(widthInput.value)
+    const height = parseInt(heightInput.value)
+    const margin = parseInt(marginInput.value)
+
+    
+    if (isNaN(height) || height <= 0 || height > 1000) {
+        shake()
+        showError("Height must be a number between 1 to 1000")
+        return 
+    }
+    
+    if (isNaN(width) || width <= 0 || width > 1000) {
+        shake()
+        showError("Width must be a number between 1 to 1000")
+        return 
+    }
+
+    if (isNaN(margin) || margin <= 0 || margin > 1000) {
+        shake()
+        showError("Margin must be a number between 1 to 100")
+        return 
+    }
+
+
 
     qrCode = new QRCodeStyling({
-        width: parseInt(width.value),
+        width: width,
 
-        height: parseInt(height.value),
+        height: height,
 
         data: data.value,
 
@@ -41,7 +78,7 @@ function generateQrCode() {
             color: bgColor.value,
         },
 
-        margin: parseInt(margin.value),
+        margin: margin,
 
         cornersSquareOptions: {
             color: squareColor.value,
@@ -74,6 +111,14 @@ imageInput.addEventListener("change", function (e) {
         return
     }
 
+    if (!file.type.startsWith("image/")) {
+        shake()
+        showError("Invalid image file. Please upload a valid image.")
+        imageInput.value = ""
+    } else {
+        showUpload()
+    }
+
     const reader = new FileReader()
     reader.readAsDataURL(file) 
 
@@ -81,10 +126,47 @@ imageInput.addEventListener("change", function (e) {
 
     reader.onload = function () {
         uploadedImageURL = reader.result
-        
-        
+
     }
 
 
 })
 
+
+
+function showError(msg) {
+    errorMessage.style.opacity = 1
+    errorMessage.innerText = msg
+}
+
+function hideError() {
+    errorMessage.style.opacity = 0
+}
+
+
+function showUpload() {
+    uploadMessage.style.opacity = 1
+        setTimeout(() => {
+            uploadMessage.style.opacity = 0
+
+        }, 3000);
+}
+
+
+
+
+function shake() {
+    container.classList.add("shake")
+    setTimeout(() => {
+        container.classList.remove("shake")
+    }, 1000);
+}
+
+
+    
+downloadBtn.addEventListener("click", function () {
+    if (!qrCode) {
+        return
+    }
+    qrCode.download({ name: "qr-code-by-MujahidRakib", extension: "png" })
+})
